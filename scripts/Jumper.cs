e3e721f337b541258d0b1c85cde2517c99a978c5
Matias <lobinux@digilearnnials.com>
2020-05-14 20:52:54 -0300

    #13 Tutorial 2

--- scripts/Jumper.cs
+++ scripts/Jumper.cs
@@ -5,19 +5,33 @@ public class Jumper : Area2D
 {
     [Export] private Vector2 velocity = new Vector2(100, 0);
     [Export(PropertyHint.Range, "100, 3000")] private float jumpSpeed = 1000;
+    [Export] private int _trailLenght = 25;
 
-    private Circle target = null;
+    private Line2D _trail;
+    
+    private Circle _target = null;
+    
+    public Circle Target => _target;
+
+    [Signal] public delegate void OnCapture(Circle circle);
 
     public override void _Ready()
     {
         Connect("area_entered", this, nameof(OnAreaEnter));
+        _trail = GetNode<Line2D>("Trail/Points");
     }
 
     public override void _PhysicsProcess(float delta)
     {
-        if (target != null)
+        if (_trail.Points.Length > _trailLenght)
         {
-            Transform = target.OrbitPosition.GlobalTransform;
+            _trail.RemovePoint(0);
+        }
+        _trail.AddPoint(Position);
+        
+        if (_target != null)
+        {
+            Transform = _target.OrbitPosition.GlobalTransform;
         }
         else
         {
@@ -27,7 +41,7 @@ public class Jumper : Area2D
 
     public override void _UnhandledInput(InputEvent @event)
     {
-        if (target != null && @event is InputEventScreenTouch screenTouch && screenTouch.Pressed)
+        if (_target != null && @event is InputEventScreenTouch screenTouch && screenTouch.Pressed)
         {
             Jump();
         }
@@ -35,13 +49,16 @@ public class Jumper : Area2D
 
     private void Jump()
     {
-        target = null;
+        _target.Implode();
+        _target = null;
         velocity = Transform.x * jumpSpeed;
     }
 
     public void OnAreaEnter(Area2D area)
     {
-        target = (Circle)area;
+        _target = (Circle)area;
+        _target.GetNode<Node2D>("Pivot").Rotation = (Position - _target.Position).Angle();
         velocity = Vector2.Zero;
+        EmitSignal(nameof(OnCapture), _target);
     }
 }
